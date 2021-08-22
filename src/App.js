f67d44dd1edcfcd62b06a5eb1cfd03df254b9e20
SonjaJan <70730267+SonjaJan@users.noreply.github.com>
2021-08-22 14:21:24 +0200

    learning functional and class components
    learning functional and class components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
+
+    
     return (
+
       <div>
-        {/* <div className="header">
-          Phone Directory
-        </div>
+        <Header />
         <button>Add</button>
         <div>
           <span>Name</span><br />
           <span>Phone</span>
-        </div> */}
-        <label htmlFor="name">Name: </label>
-        <input id="name" type="text" placeholder="Type Here" defaultValue="Sonja"/>
+        </div> 
+    
       </div>
     );
+    
   }
 }
+
+
 
 export default App;
