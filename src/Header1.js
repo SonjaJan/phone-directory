bf8f64217623ba848f268d5e0f7f4ca9dec61dec
SonjaJan <70730267+SonjaJan@users.noreply.github.com>
2021-08-26 17:09:52 +0200

    Styled ‘Header’ (External Styling)

--- src/Header1.js
+++ src/Header1.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './Header.css'
 
 const Header1 = function(){
-    const headerStyle = {textAlign:'center', padding:20, background:'#000',color:'#fff', textTransform:'uppercase'};
     return(
-        <div style={headerStyle}>
+        <div className='header'>
         Phone Directory
       </div>
     )
